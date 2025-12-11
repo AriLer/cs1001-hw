97d239fbebd31001b6b60c90bb90c7e09144a16f
Ari <ari.lerner1@gmail.com>
2025-12-11 22:10:14 +0200

    question 5 1

--- hw3/hw3_326126877.py
+++ hw3/hw3_326126877.py
@@ -111,12 +111,107 @@ def sort_strings2(lst, k):
 
 # Q4_2c
 def find_almost_1(lst, s):
-    pass  # TODO replace with your code
+    low, high = 0, len(lst) - 1
 
+    while low <= high:
+        mid = (low + high) // 2
+        if lst[mid] == s:
+            return mid
+        if mid - 1 >= 0 and lst[mid - 1] == s:
+            return mid - 1
+        if mid + 1 < len(lst) and lst[mid + 1] == s:
+            return mid + 1
+        if s < lst[mid]:
+            high = mid - 2
+        else:
+            low = mid + 2
+
+    return None
 
 # Q4_3a
 def find_percentile_almost_k(lst, k, m):
-    pass  # TODO replace with your code
+    target_global_rank = m - 1
+
+    # Determine the window where the correct element can appear
+    sub_lst_start = max(0, target_global_rank - k)
+    sub_lst_end   = min(len(lst) - 1, target_global_rank + k)
+
+    # Extract the relevant part of the list (size ≤ 2k+1)
+    sub_lst = lst[sub_lst_start : sub_lst_end + 1]
+    
+    # The target rank inside the window
+    target_local_rank = target_global_rank - sub_lst_start
+
+
+    # -------- Inlined Quickselect Algorithm --------
+    left = 0
+    right = len(sub_lst) - 1
+
+    while True:
+        if left == right:
+            return sub_lst[left]
+        
+        mid = sub_lst[(left + right) // 2]
+
+        # Partition the window around the pivot
+        i, j = left, right
+        while i <= j:
+            while sub_lst[i] < mid:
+                i += 1
+            while sub_lst[j] > mid:
+                j -= 1
+            if i <= j:
+                sub_lst[i], sub_lst[j] = sub_lst[j], sub_lst[i]
+                i += 1
+                j -= 1
+
+        # Decide which side contains the target rank
+        if target_local_rank <= j:
+            right = j
+        elif target_local_rank >= i:
+            left = i
+        else:
+            return sub_lst[target_local_rank]
+
+
+# def sort_almost_k(lst, k):
+#     n = len(lst)
+
+#     for i in range(n-k):
+#         min_index = i
+#         for j in range(i + 1, i + k + 1):
+#             if lst[min_index] > lst[j]:
+#                 min_index = j
+#         tmp = lst[i]
+#         lst[i] = lst[min_index]
+#         lst[min_index] = tmp
+        
+#     for i in range (n - k,n):
+#         min_index = i
+#         for j in range(i + 1, n):
+#             if lst[min_index] > lst[j]:
+#                 min_index = j
+#         tmp = lst[i]
+#         lst[i] = lst[min_index]
+#         lst[min_index] = tmp
+        
+        
+# def find_percentile_almost_k(lst, k, m):
+#     n = len(lst)
+#     left = max(m - 1 - k, 0)
+#     right = min(m + k, n)
+
+#     if n == 0:
+#         return None
+
+#     if m == 0:
+#         m = 1
+
+#     #if m!=0:
+#     relevant_lst = lst[left:right]
+#     sort_almost_k(relevant_lst, k)
+#     return relevant_lst[m - 1 - left]
+
 
 
 ##############
@@ -138,7 +233,18 @@ def edit_distance(st1, st2):
 
 # Q5_a
 def relevancy_score(text, promote, L):
-    pass  # TODO replace with your code
+    score = 1
+    if promote:
+        score = 2
+    
+    min_distance = edit_distance(text, L[0])
+    for keyword in L:
+        distance = edit_distance(text, keyword)
+        if distance < min_distance:
+            min_distance = distance
+            
+    score = score* (1 / (1 + (min_distance**2)))
+    return score
 
 
 # Q5_b
@@ -166,8 +272,6 @@ def test():
     if sort_strings1(lst1, 4) \
             != ['aacc', 'aacc', 'adae', 'aede', 'becb', 'cccc', 'ccea', 'daea', 'dded', 'deea', 'edea']:
         print("error in sort_strings1")
-
-    print(sort_strings2(lst1, 4))
     if sort_strings2(lst1, 4) \
             != ['aacc', 'aacc', 'adae', 'aede', 'becb', 'cccc', 'ccea', 'daea', 'dded', 'deea', 'edea']:
         print("error in sort_strings2")

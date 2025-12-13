d0a87a8ab7f58c493b0acdd6c1e11146c7f35427
Ari <ari.lerner1@gmail.com>
2025-12-13 23:15:39 +0200

    finish euqestions 2-6

--- hw3/hw3_326126877.py
+++ hw3/hw3_326126877.py
@@ -15,30 +15,29 @@ import random  # loads python's random module to use in Q5
 
 # Q2_a
 
-def find_root(f, L, U, eps=10**-1000):
-    """ Find root of f using intermediate value theorem. 
-        Assume f is continuous, L<U and f(L) < 0 < f(U).
-        eps is how far you allow f to be from 0.0
-    """ 
-    assert L<U and f(L)<0 and f(U)>0
 
-    M = (L+U)/2
-    while L<M and M<U:
+def find_root(f, L, U, eps=10**-1000):
+    """Find root of f using intermediate value theorem.
+    Assume f is continuous, L<U and f(L) < 0 < f(U).
+    eps is how far you allow f to be from 0.0
+    """
+    assert L < U and f(L) < 0 and f(U) > 0
+
+    M = (L + U) / 2
+    while L < M and M < U:
         fM = f(M)
         print("searching in (", L, ",", U, ")")
         if abs(fM) < eps:
             print("Found an approximated root")
             return M
         elif fM < 0:
-            L = M # continue search in upper half
-        else: # fM > 0
-            U = M # continue search in lower half
-        M = (L+U)/2
-        
+            L = M  # continue search in upper half
+        else:  # fM > 0
+            U = M  # continue search in lower half
+        M = (L + U) / 2
+
     # if we got here no root was found (try increasing eps)
     return None
-
-# find_root(lambda x: x**2 - 4, 0, 3)
 
 
 # Q2_f
@@ -49,14 +48,13 @@ def find_log(m, eps=10**-10):
     return log
 
 
-
 ##############
 # QUESTION 3 #
 ##############
 # Q3_a
-def string_to_int(s):    
-    char_dict ={ 'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4}
-    total_value = 0 
+def string_to_int(s):
+    char_dict = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4}
+    total_value = 0
     for i in range(len(s)):
         total_value = (total_value * 5) + char_dict[s[i]]
     return total_value
@@ -64,9 +62,9 @@ def string_to_int(s):
 
 # Q3_b
 def int_to_string(k, n):
-    char_dict ={ 'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4}
-    str = ['a'] * k
-    
+    char_dict = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4}
+    str = ["a"] * k
+
     for i in range(k - 1, -1, -1):
         remainder = n % 5
         for key, value in char_dict.items():
@@ -74,15 +72,16 @@ def int_to_string(k, n):
                 str[i] = key
                 break
         n = n // 5
-        
+
     return "".join(str)
+
 
 # Q3_c
 def sort_strings1(lst, k):
-    help_lst = [""]*(5 ** k)
+    help_lst = [""] * (5**k)
     for s in lst:
         index = string_to_int(s)
-        if(help_lst[index] == ""):
+        if help_lst[index] == "":
             help_lst[index] = s
         else:
             help_lst.insert(index, s)
@@ -90,8 +89,9 @@ def sort_strings1(lst, k):
     for s in help_lst:
         if s != "":
             sorted_lst.append(s)
-            
+
     return sorted_lst
+
 
 # Q3_e
 def sort_strings2(lst, k):
@@ -102,12 +102,12 @@ def sort_strings2(lst, k):
             if index == i:
                 sorted_lst.append(s)
     return sorted_lst
-    
 
 
 ##############
 # QUESTION 4 #
 ##############
+
 
 # Q4_2c
 def find_almost_1(lst, s):
@@ -125,35 +125,27 @@ def find_almost_1(lst, s):
             high = mid - 2
         else:
             low = mid + 2
-
     return None
+
 
 # Q4_3a
 def find_percentile_almost_k(lst, k, m):
-    target_global_rank = m - 1
-
-    # Determine the window where the correct element can appear
-    sub_lst_start = max(0, target_global_rank - k)
-    sub_lst_end   = min(len(lst) - 1, target_global_rank + k)
-
-    # Extract the relevant part of the list (size ≤ 2k+1)
-    sub_lst = lst[sub_lst_start : sub_lst_end + 1]
+    global_smaller_count = m - 1
+    sub_lst_start = max(0, global_smaller_count - k)
+    sub_lst_end = min(len(lst) - 1, global_smaller_count + k)
     
-    # The target rank inside the window
-    target_local_rank = target_global_rank - sub_lst_start
+    sub_lst = lst[sub_lst_start : sub_lst_end + 1]
+    local_smaller_count = global_smaller_count - sub_lst_start
 
-
-    # -------- Inlined Quickselect Algorithm --------
     left = 0
     right = len(sub_lst) - 1
 
     while True:
         if left == right:
             return sub_lst[left]
-        
+
         mid = sub_lst[(left + right) // 2]
 
-        # Partition the window around the pivot
         i, j = left, right
         while i <= j:
             while sub_lst[i] < mid:
@@ -164,54 +156,13 @@ def find_percentile_almost_k(lst, k, m):
                 sub_lst[i], sub_lst[j] = sub_lst[j], sub_lst[i]
                 i += 1
                 j -= 1
-
-        # Decide which side contains the target rank
-        if target_local_rank <= j:
+                
+        if local_smaller_count <= j:
             right = j
-        elif target_local_rank >= i:
+        elif local_smaller_count >= i:
             left = i
         else:
-            return sub_lst[target_local_rank]
-
-
-# def sort_almost_k(lst, k):
-#     n = len(lst)
-
-#     for i in range(n-k):
-#         min_index = i
-#         for j in range(i + 1, i + k + 1):
-#             if lst[min_index] > lst[j]:
-#                 min_index = j
-#         tmp = lst[i]
-#         lst[i] = lst[min_index]
-#         lst[min_index] = tmp
-        
-#     for i in range (n - k,n):
-#         min_index = i
-#         for j in range(i + 1, n):
-#             if lst[min_index] > lst[j]:
-#                 min_index = j
-#         tmp = lst[i]
-#         lst[i] = lst[min_index]
-#         lst[min_index] = tmp
-        
-        
-# def find_percentile_almost_k(lst, k, m):
-#     n = len(lst)
-#     left = max(m - 1 - k, 0)
-#     right = min(m + k, n)
-
-#     if n == 0:
-#         return None
-
-#     if m == 0:
-#         m = 1
-
-#     #if m!=0:
-#     relevant_lst = lst[left:right]
-#     sort_almost_k(relevant_lst, k)
-#     return relevant_lst[m - 1 - left]
-
+            return sub_lst[local_smaller_count]
 
 
 ##############
@@ -236,31 +187,52 @@ def relevancy_score(text, promote, L):
     score = 1
     if promote:
         score = 2
-    
+
     min_distance = edit_distance(text, L[0])
     for keyword in L:
         distance = edit_distance(text, keyword)
         if distance < min_distance:
             min_distance = distance
-            
-    score = score* (1 / (1 + (min_distance**2)))
+
+    score = score * (1 / (1 + (min_distance**2)))
     return score
 
 
 # Q5_b
-def PageRank_search(G, t, p, text, pages_desc, pages_promote):
-    pass  # TODO replace with your code
+import random
 
+
+def PageRank_search(G, t, p, text, pages_desc, pages_promote):
+    relevancies = [
+        relevancy_score(text, pages_promote[i], pages_desc[i]) for i in range(len(G))
+    ]
+    counter = [0] * len(G)
+    curr = 0
+
+    for turn in range(t):
+        counter[curr] += 1
+
+        if G[curr] and random.random() < p:
+            neighbors = G[curr]
+            weights = [relevancies[v] for v in neighbors]
+            curr = random.choices(neighbors, weights=weights)[0]
+        else:
+            curr = random.choices(range(len(G)), weights=relevancies)[0]
+
+    # נרמול הביקורים
+    total_visits = sum(counter)
+    return [v / total_visits for v in counter]
 
 ##########
 # Tester #
 ##########
 
+
 def test():
     # Q3
     if string_to_int("aa") != 0 or string_to_int("aba") != 5:
         print("error in string_to_int")
-    lst_num = [random.choice(range(5 ** 4)) for i in range(15)]
+    lst_num = [random.choice(range(5**4)) for i in range(15)]
     for i in lst_num:
         s = int_to_string(4, i)
         if s is None or len(s) != 4:
@@ -268,16 +240,50 @@ def test():
         if string_to_int(s) != i:
             print("error in int_to_string and/or in string_to_int")
 
-    lst1 = ['aede', 'adae', 'dded', 'deea', 'cccc', 'aacc', 'edea', 'becb', 'daea', 'ccea', 'aacc']
-    if sort_strings1(lst1, 4) \
-            != ['aacc', 'aacc', 'adae', 'aede', 'becb', 'cccc', 'ccea', 'daea', 'dded', 'deea', 'edea']:
+    lst1 = [
+        "aede",
+        "adae",
+        "dded",
+        "deea",
+        "cccc",
+        "aacc",
+        "edea",
+        "becb",
+        "daea",
+        "ccea",
+        "aacc",
+    ]
+    if sort_strings1(lst1, 4) != [
+        "aacc",
+        "aacc",
+        "adae",
+        "aede",
+        "becb",
+        "cccc",
+        "ccea",
+        "daea",
+        "dded",
+        "deea",
+        "edea",
+    ]:
         print("error in sort_strings1")
-    if sort_strings2(lst1, 4) \
-            != ['aacc', 'aacc', 'adae', 'aede', 'becb', 'cccc', 'ccea', 'daea', 'dded', 'deea', 'edea']:
+    if sort_strings2(lst1, 4) != [
+        "aacc",
+        "aacc",
+        "adae",
+        "aede",
+        "becb",
+        "cccc",
+        "ccea",
+        "daea",
+        "dded",
+        "deea",
+        "edea",
+    ]:
         print("error in sort_strings2")
 
     # Q4
-    almost_sorted_lst_3 = [10,7,7,1]
+    almost_sorted_lst_3 = [10, 7, 7, 1]
     if find_percentile_almost_k(almost_sorted_lst_3, 3, 1) != 1:
         print("error in find_percentile_almost_k")
     if find_percentile_almost_k(almost_sorted_lst_3, 3, 2) != 7:
@@ -292,5 +298,8 @@ def test():
     if relevancy_score("spotr", True, ["sport", "gym", "workout"]) != 0.4:
         print("error in relevancy_score")
 
+
 test()
 
+
+# השאלה הבאה נתנה לי בעבודה. תממש את הפונקציה הנדרשת בסעיף ב' תוך כדי שימוש בפונקציה

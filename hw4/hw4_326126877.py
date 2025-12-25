f2405f4dabb2aeabc98ce864de72c75f27a327c2
Ari <ari.lerner1@gmail.com>
2025-12-25 14:13:32 +0200

    finish question 2

--- hw4/hw4_326126877.py
+++ hw4/hw4_326126877.py
@@ -12,8 +12,9 @@ import math, random, time
 # Question 1 #
 ##############
 
+
 def quicksort(lst, rand=True):
-    """ quick sort of lst """
+    """quick sort of lst"""
     if len(lst) <= 1:
         return lst
     else:
@@ -28,63 +29,85 @@ def quicksort(lst, rand=True):
 def quick_comparison_random_input(n, t):
     deterministic_time = 0
     random_time = 0
-    
+
     for _ in range(t):
         random_list = list(range(1, n + 1))
         random.shuffle(random_list)
-        
+
         start_time = time.perf_counter()
         quicksort(random_list, rand=False)
         end_time = time.perf_counter()
-        deterministic_time += (end_time - start_time)
+        deterministic_time += end_time - start_time
 
         start_time = time.perf_counter()
         quicksort(random_list, rand=True)
         end_time = time.perf_counter()
-        random_time += (end_time - start_time)
+        random_time += end_time - start_time
 
     return (random_time / t, deterministic_time / t)
+
 
 def quick_comparison_ordered_input(n, t):
     deterministic_time = 0
     random_time = 0
-    
+
     for _ in range(t):
         ordered_list = list(range(1, n + 1))
         start_time = time.perf_counter()
 
         quicksort(ordered_list, rand=False)
         end_time = time.perf_counter()
-        deterministic_time += (end_time - start_time)
+        deterministic_time += end_time - start_time
 
         start_time = time.perf_counter()
         quicksort(ordered_list, rand=True)
         end_time = time.perf_counter()
-        random_time += (end_time - start_time)
+        random_time += end_time - start_time
 
     return (random_time / t, deterministic_time / t)
-
-print(quick_comparison_ordered_input(300, 10))
 
 ##############
 # Question 2 #
 ##############
-#2b
-def max_v1_improved(L):
-    pass  # replace with your code
+
+# TODO: Question 2 - a (drawing the binary recursion tree)
+# 2b
+def init_max_v1_improved(L):
+    def max_v1_improved(start, end):
+        if start == end:
+            return L[start]
+
+        mid = (start + end) // 2
+        first_half = max_v1_improved(start, mid)
+        second_half = max_v1_improved(mid + 1, end)
+
+        return max(first_half, second_half)
+
+    return max_v1_improved(0, len(L) - 1)
 
 
-def max_v2_improved(L):
-    pass  # replace with your code
+def init_max_v2_improved(L):
 
-#2c
+    def max_v2_improved(start):
+        if start == len(L) - 1:
+            return L[start]
+       
+        without_left = max_v2_improved(start + 1) 
+        return max(without_left, L[start])
+
+    return max_v2_improved(0)
+
+# 2c
 def reverse(L):
-    pass  # replace with your code
-
+    reversed = []
+    for i in range(len(L) - 1, -1 ,-1):
+        reversed.append(L[i])
+    return reversed
 
 ##############
 # Question 3 #
 ##############
+
 
 def cnt_paths(L):
     if all([elem == 0 for elem in L]):
@@ -98,18 +121,33 @@ def cnt_paths(L):
             L[i] += 1
     return result
 
-
-def cnt_paths_mem(L):
+def cnt_paths_mem(L, memo={}):
+    if tuple(L) in memo:
+        return memo[tuple(L)]
+    
     if all([elem == 0 for elem in L]):
         return 1
-
+    
     result = 0
     for i in range(len(L)):
         if L[i] != 0:
             L[i] -= 1
-            result += cnt_paths(L)
+            memo[tuple(L)] = cnt_paths_mem(L, memo)
             L[i] += 1
     return result
+
+def test_cnt_paths():
+    # Base Case: Already at the origin
+    assert cnt_paths_mem([0, 0]) == 1
+    
+    # Single Dimension: Only 1 path possible (straight line)
+    print("cnt_paths_mem([5]): ", cnt_paths_mem([5]))
+    assert cnt_paths_mem([5]) == 1
+
+    # 2D Grid (2x2): Should be 6 paths (4 choose 2)
+    # Paths: (1,1,0,0) permutations: RRYY, RYRY, RYYR, YRYR, YRRY, YYRR
+    assert cnt_paths_mem([2, 2]) == 6
+test_cnt_paths()
 
 
 def cnt_paths_iter(L):
@@ -143,11 +181,12 @@ def path_v4(A, s, t):
     L = [False for i in range(len(A))]
     return path_rec(A, s, t, L)
 
+
 def path_rec(A, s, t, L):
     if s == t:
         return True
 
-    for i in range(len(A)):    
+    for i in range(len(A)):
         if A[s][i] == 1:
             if path_rec(A, i, t, L):
                 return True
@@ -162,12 +201,14 @@ def path_rec(A, s, t, L):
 
 # 5a
 
+
 def can_create_once(s, lst):
 
     pass  # replace this with your code
 
 
 # 5b
+
 
 def can_create_twice(s, lst):
 
@@ -176,10 +217,9 @@ def can_create_twice(s, lst):
 
 # 5c
 
+
 def valid_brackets_placement(s, lst):
     pass  # replace this with your code
-
-
 
 
 ##########
@@ -207,25 +247,43 @@ def test():
         print("error in cnt_pths_mem")
 
     # 4a
-    A = [[0, 1, 1, 0, 0], [1, 0, 1, 0, 0], [0, 0, 0, 1, 0], [1, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
-    if not legal_path(A.copy(), [0, 1, 2, 3]) or \
-            not legal_path(A.copy(), [0, 1, 2, 3, 0, 1]) or \
-            legal_path(A.copy(), [1, 2, 3, 4]):
+    A = [
+        [0, 1, 1, 0, 0],
+        [1, 0, 1, 0, 0],
+        [0, 0, 0, 1, 0],
+        [1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ]
+    if (
+        not legal_path(A.copy(), [0, 1, 2, 3])
+        or not legal_path(A.copy(), [0, 1, 2, 3, 0, 1])
+        or legal_path(A.copy(), [1, 2, 3, 4])
+    ):
         print("error in legal_path")
 
     # 5a
-    if not can_create_once(6, [5, 2, 3]) or not can_create_once(-10, [5, 2, 3]) \
-            or can_create_once(9, [5, 2, 3]) or can_create_once(7, [5, 2, 3]):
+    if (
+        not can_create_once(6, [5, 2, 3])
+        or not can_create_once(-10, [5, 2, 3])
+        or can_create_once(9, [5, 2, 3])
+        or can_create_once(7, [5, 2, 3])
+    ):
         print("error in can_create_once")
 
     # 5b
-    if not can_create_twice(6, [5, 2, 3]) or not can_create_twice(9, [5, 2, 3]) \
-            or not can_create_twice(7, [5, 2, 3]) or can_create_once(19, [5, 2, 3]):
+    if (
+        not can_create_twice(6, [5, 2, 3])
+        or not can_create_twice(9, [5, 2, 3])
+        or not can_create_twice(7, [5, 2, 3])
+        or can_create_once(19, [5, 2, 3])
+    ):
         print("error in can_create_twice")
 
     # 5c
-    lst = ['6', '-', '4', '*', '2', '+', '3']
-    if not valid_brackets_placement(10, lst.copy()) or \
-            not valid_brackets_placement(1, lst.copy()) or \
-            valid_brackets_placement(5, lst.copy()):
+    lst = ["6", "-", "4", "*", "2", "+", "3"]
+    if (
+        not valid_brackets_placement(10, lst.copy())
+        or not valid_brackets_placement(1, lst.copy())
+        or valid_brackets_placement(5, lst.copy())
+    ):
         print("error in valid_brackets_placement")
